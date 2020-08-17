Fix, test 2-norm computation: do not take absolute values

grudge/symbolic/operators.py
@@ -721,7 +721,7 @@ def norm(p, arg, dd=None):
 
     if p == 2:
         norm_squared = NodalSum(dd_in=dd)(
-                prim.fabs(arg * MassOperator()(arg)))
+                arg * MassOperator()(arg))
 
         if isinstance(norm_squared, np.ndarray):
             if len(norm_squared.shape) != 1:

test/test_grudge.py
@@ -669,7 +669,7 @@ def test_norm_obj_array(ctx_factory, p):
 
     norm_exact = w[0]
     logger.info("norm: %.5e %.5e", norm, norm_exact)
-    # assert abs(norm - norm_exact) < 1.0e-14
+    assert abs(norm - norm_exact) < 1.0e-14
 
     # }}}
 
@@ -680,7 +680,7 @@ def test_norm_obj_array(ctx_factory, p):
 
     norm_exact = np.sqrt(np.sum(w**2)) if p == 2 else np.max(w)
     logger.info("norm: %.5e %.5e", norm, norm_exact)
-    # assert abs(norm - norm_exact) < 1.0e-14
+    assert abs(norm - norm_exact) < 1.0e-14
 
     # }}}
 