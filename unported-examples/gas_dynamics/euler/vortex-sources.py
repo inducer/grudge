9641231193e0e3e750ed30c5fd471da28393f80b
Andreas Kloeckner <inform@tiker.net>
2020-09-04 18:50:21 -0500

    Eradicate last traces of GPL licensing

--- unported-examples/gas_dynamics/euler/vortex-sources.py
+++ unported-examples/gas_dynamics/euler/vortex-sources.py
@@ -1,18 +1,24 @@
-# Copyright (C) 2008 Andreas Kloeckner
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+__copyright__ = "Copyright (C) 2008 Andreas Kloeckner"
 
+__license__ = """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
 
 
 
@@ -90,7 +96,7 @@ class SourceTerms:
         x_rel = x_vec[0] - vortex_loc[0]
         y_rel = x_vec[1] - vortex_loc[1]
 
-        # sources written in terms of A=1.0 solution 
+        # sources written in terms of A=1.0 solution
         # (standard isentropic vortex)
 
         from math import pi
@@ -148,7 +154,7 @@ def main(write_output=True):
 
     gamma = 1.4
 
-    # at A=1 we have case of isentropic vortex, source terms 
+    # at A=1 we have case of isentropic vortex, source terms
     # arise for other values
     densityA = 2.0
 
@@ -191,7 +197,7 @@ def main(write_output=True):
         from grudge.mesh import BTAG_ALL
 
         op = GasDynamicsOperator(dimensions=2,
-                mu=0.0, prandtl=0.72, spec_gas_const=287.1, 
+                mu=0.0, prandtl=0.72, spec_gas_const=287.1,
                 equation_of_state=GammaLawEOS(vortex.gamma),
                 bc_inflow=vortex, bc_outflow=vortex, bc_noslip=vortex,
                 inflow_tag=BTAG_ALL, source=sources)
