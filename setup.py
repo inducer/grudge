cc431b2682441bf97eabc6cf3ce28a29b24d7e4d
Christensen <njchris2@illinois.edu>
2021-01-17 18:56:16 -0600

    Add gmsh to requirements

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ def main():
               "leap>=2019.1",
               "dagrt>=2019.1",
               "hjson",
+              "gmsh",
               "import_resources; python_version<'3.7'"
               ])
 
