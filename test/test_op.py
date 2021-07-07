059e9b7f667c181ba285eb593da1c244a5b81c1e
Matthias Diener <mdiener@illinois.edu>
2021-07-07 17:32:24 -0500

    test_op fix

--- test/test_op.py
+++ test/test_op.py
@@ -271,6 +271,9 @@ def test_divergence(actx_factory, form, dim, order, vectorize, nested,
         rel_linf_err = (
             op.norm(dcoll, div_u - expected_div_u, np.inf)
             / op.norm(dcoll, expected_div_u, np.inf))
+
+        if not np.isscalar(rel_linf_err):
+            rel_linf_err = actx.to_numpy(rel_linf_err)
         eoc_rec.add_data_point(1./n, rel_linf_err)
 
     print("L^inf error:")
