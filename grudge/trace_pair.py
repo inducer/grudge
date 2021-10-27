76f0f733f3c19c4d9bf57c4c8bfd2d054bfd24bc
Thomas Gibson <gibsonthomas1120@hotmail.com>
2021-10-27 13:16:13 -0500

    Clean up comments related to rank-communication for trace pairs

--- grudge/trace_pair.py
+++ grudge/trace_pair.py
@@ -288,7 +288,6 @@ class _RankBoundaryCommunication:
         btag = BTAG_PARTITION(remote_rank)
 
         local_bdry_data = project(dcoll, "vol", btag, array_container)
-        local_bdry_data_np = to_numpy(flatten(local_bdry_data, actx), actx)
         comm = dcoll.mpi_communicator
 
         self.dcoll = dcoll
@@ -296,37 +295,43 @@ class _RankBoundaryCommunication:
         self.remote_btag = btag
         self.bdry_discr = dcoll.discr_from_dd(btag)
         self.local_bdry_data = local_bdry_data
+        self.local_bdry_data_np = \
+            to_numpy(flatten(self.local_bdry_data, actx), actx)
 
         self.tag = self.base_tag
         if tag is not None:
             self.tag += tag
 
-        # Here, we create initialize both send and recieve operations through
+        # Here, we initialize both send and recieve operations through
         # mpi4py `Request` (MPI_Request) instances for comm.Isend (MPI_Isend)
         # and comm.Irecv (MPI_Irecv) respectively. These initiate non-blocking
         # point-to-point communication requests and require explicit management
         # via the use of wait (MPI_Wait, MPI_Waitall, MPI_Waitany, MPI_Waitsome),
         # test (MPI_Test, MPI_Testall, MPI_Testany, MPI_Testsome), and cancel
-        # (MPI_Cancel). Currently, the rank-local data `local_bdry_data_np`
-        # will have its memory buffer exposed by initiating the `Isend` request,
-        # and will remain so until the send request is complete. As a result,
-        # one must be careful to not access the data at the Python level during
-        # this nonblocking communication process. Completion of the requests
-        # are handled in :meth:`finish`.
+        # (MPI_Cancel). The rank-local data `self.local_bdry_data_np` will have its
+        # associated memory buffer sent across connected ranks and must not be
+        # modified at the Python level during this process. Completion of the
+        # requests are handled in :meth:`finish`.
         #
         # For more details on the mpi4py semantics, see:
         # https://mpi4py.readthedocs.io/en/stable/overview.html#nonblocking-communications
-        self.send_req = comm.Isend(local_bdry_data_np, remote_rank, tag=self.tag)
-        self.remote_data_host_numpy = np.empty_like(local_bdry_data_np)
+        #
+        # NOTE: mpi4py does hold onto the memory buffer for `self.local_bdry_data_np`
+        # until the send/recv requests are complete.
+        self.send_req = comm.Isend(self.local_bdry_data_np,
+                                   remote_rank,
+                                   tag=self.tag)
+        self.remote_data_host_numpy = np.empty_like(self.local_bdry_data_np)
         self.recv_req = comm.Irecv(self.remote_data_host_numpy,
-                                   remote_rank, self.tag)
+                                   remote_rank,
+                                   tag=self.tag)
 
     def finish(self):
         # Wait for the nonblocking recieve request to complete before
         # accessing the data
         self.recv_req.Wait()
 
-        # Nonblocking recieve is complete, we can now acces the data and apply
+        # Nonblocking recieve is complete, we can now access the data and apply
         # the boundary-swap connection
         actx = self.array_context
         remote_bdry_data_flat = from_numpy(self.remote_data_host_numpy, actx)
@@ -336,8 +341,8 @@ class _RankBoundaryCommunication:
             dof_desc.as_dofdesc(dof_desc.DTAG_BOUNDARY(self.remote_btag)))
         swapped_remote_bdry_data = bdry_conn(remote_bdry_data)
 
-        # Complete the nonblocking send request and close the memory buffer
-        # associated with `self.remote_data_host_numpy`
+        # Complete the nonblocking send request associated with communicating
+        # `self.local_bdry_data_np`
         self.send_req.Wait()
 
         return TracePair(self.remote_btag,
@@ -369,7 +374,7 @@ def cross_rank_trace_pairs(
         return [TracePair(BTAG_PARTITION(remote_rank), interior=ary, exterior=ary)
                 for remote_rank in connected_ranks(dcoll)]
 
-    # Initializing and posting all sends/receives
+    # Initialize and post all sends/receives
     rank_bdry_communcators = [
         _RankBoundaryCommunication(dcoll, ary, remote_rank, tag=tag)
         for remote_rank in connected_ranks(dcoll)
