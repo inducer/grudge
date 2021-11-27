29e2256b809a382e5b7413e21450718146c57aa4
Matthias Diener <mdiener@illinois.edu>
2021-11-26 18:41:27 -0600

    add lazy option to wave-op-mpi (#185)

--- examples/wave/wave-op-mpi.py
+++ examples/wave/wave-op-mpi.py
@@ -31,8 +31,8 @@ import numpy.linalg as la  # noqa
 import pyopencl as cl
 import pyopencl.tools as cl_tools
 
-from arraycontext import thaw
-from grudge.array_context import PyOpenCLArrayContext
+from arraycontext import thaw, freeze
+from grudge.array_context import PytatoPyOpenCLArrayContext, PyOpenCLArrayContext
 
 from pytools.obj_array import flat_obj_array
 
@@ -141,14 +141,18 @@ def bump(actx, dcoll, t=0):
             / source_width**2))
 
 
-def main(ctx_factory, dim=2, order=3, visualize=False):
+def main(ctx_factory, dim=2, order=3, visualize=False, lazy=False):
     cl_ctx = ctx_factory()
     queue = cl.CommandQueue(cl_ctx)
-    actx = PyOpenCLArrayContext(
-        queue,
-        allocator=cl_tools.MemoryPool(cl_tools.ImmediateAllocator(queue)),
-        force_device_scalars=True,
-    )
+
+    if lazy:
+        actx = PytatoPyOpenCLArrayContext(queue)
+    else:
+        actx = PyOpenCLArrayContext(
+            queue,
+            allocator=cl_tools.MemoryPool(cl_tools.ImmediateAllocator(queue)),
+            force_device_scalars=True,
+        )
 
     comm = MPI.COMM_WORLD
     num_parts = comm.Get_size()
@@ -192,6 +196,8 @@ def main(ctx_factory, dim=2, order=3, visualize=False):
     def rhs(t, w):
         return wave_operator(dcoll, c=c, w=w)
 
+    compiled_rhs = actx.compile(rhs)
+
     if comm.rank == 0:
         logger.info("dt = %g", dt)
 
@@ -199,7 +205,10 @@ def main(ctx_factory, dim=2, order=3, visualize=False):
     t_final = 3
     istep = 0
     while t < t_final:
-        fields = rk4_step(fields, t, dt, rhs)
+        if lazy:
+            fields = thaw(freeze(fields, actx), actx)
+
+        fields = rk4_step(fields, t, dt, compiled_rhs)
 
         l2norm = actx.to_numpy(op.norm(dcoll, fields[0], 2))
 
@@ -238,12 +247,16 @@ if __name__ == "__main__":
     parser.add_argument("--dim", default=2, type=int)
     parser.add_argument("--order", default=3, type=int)
     parser.add_argument("--visualize", action="store_true")
+    parser.add_argument("--lazy", action="store_true",
+                        help="switch to a lazy computation mode")
+
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
     main(cl.create_some_context,
          dim=args.dim,
          order=args.order,
-         visualize=args.visualize)
+         visualize=args.visualize,
+         lazy=args.lazy)
 
 # vim: foldmethod=marker
