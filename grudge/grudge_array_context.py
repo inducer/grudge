120f108aff335b32cd09bed9653731b6b95cca4a
Nicholas Christensen <nicholas.j.christensen@outlook.com>
2022-02-07 20:45:30 -0500

    update actx_special calling code

--- grudge/grudge_array_context.py
+++ grudge/grudge_array_context.py
@@ -241,6 +241,40 @@ class GrudgeFakeNumpyNamespace(PyOpenCLFakeNumpyNamespace):
 
     def __getattr__(self, name):
         def loopy_implemented_elwise_func(*args):
+            if all(np.isscalar(ary) for ary in args):
+                return getattr(
+                         np, self._c_to_numpy_arc_functions.get(name, name)
+                         )(*args)
+            actx = self._array_context
+            prg = _get_scalar_func_loopy_program(actx,
+                    c_name, nargs=len(args), naxes=len(args[0].shape))
+            outputs = actx.call_loopy(prg,
+                    **{"inp%d" % i: arg for i, arg in enumerate(args)})
+            exit()
+            return outputs["out"]
+
+        if name in self._c_to_numpy_arc_functions:
+            from warnings import warn
+            warn(f"'{name}' in ArrayContext.np is deprecated. "
+                    f"Use '{self._c_to_numpy_arc_functions[name]}' as in numpy. "
+                    "The old name will stop working in 2022.",
+                    DeprecationWarning, stacklevel=3)
+
+        # normalize to C names anyway
+        c_name = self._numpy_to_c_arc_functions.get(name, name)
+
+        # limit which functions we try to hand off to loopy
+        if (name in self._numpy_math_functions
+                or name in self._c_to_numpy_arc_functions):
+            return multimapped_over_array_containers(loopy_implemented_elwise_func)
+        else:
+            raise AttributeError(
+                    f"'{type(self._array_context).__name__}.np' object "
+                    f"has no attribute '{name}'")
+
+    """ Old version
+    def __getattr__(self, name):
+        def loopy_implemented_elwise_func(*args):
             actx = self._array_context
             prg = _get_scalar_func_loopy_program(actx,
                     c_name, nargs=len(args), naxes=len(args[0].shape))
@@ -263,6 +297,7 @@ class GrudgeFakeNumpyNamespace(PyOpenCLFakeNumpyNamespace):
             return multimapped_over_array_containers(loopy_implemented_elwise_func)
         else:
             raise AttributeError(name)
+    """
 
 # The PyOpenCLArrayContext needs this since the array dimensions are
 # Maybe the parameter fixing should be moved into the PyOpenCLArrayContext
