60d4fa0d8334ba439b21e1cd83c0fcb86ae94890
Andreas Kloeckner <inform@tiker.net>
2022-03-27 21:37:54 -0500

    Rework *map_subarrays docs, add types, use leaf_cls

--- grudge/op.py
+++ grudge/op.py
@@ -270,9 +270,7 @@ def local_grad(
         f=partial(_strong_scalar_grad, dcoll, dd_in),
         in_shape=(),
         out_shape=(dcoll.ambient_dim,),
-        is_scalar=lambda v: isinstance(v, DOFArray),
-        return_nested=nested,
-        ary=vec)
+        ary=vec, leaf_cls=DOFArray, return_nested=nested,)
 
 
 def local_d_dx(
@@ -330,8 +328,7 @@ def local_div(dcoll: DiscretizationCollection, vecs) -> ArrayOrContainerT:
             for i, vec_i in enumerate(vec)),
         in_shape=(dcoll.ambient_dim,),
         out_shape=(),
-        is_scalar=lambda v: isinstance(v, DOFArray),
-        ary=vecs)
+        ary=vecs, leaf_cls=DOFArray)
 
 # }}}
 
@@ -435,9 +432,7 @@ def weak_local_grad(
         f=partial(_weak_scalar_grad, dcoll, dd_in),
         in_shape=(),
         out_shape=(dcoll.ambient_dim,),
-        is_scalar=lambda v: isinstance(v, DOFArray),
-        return_nested=nested,
-        ary=vecs)
+        ary=vecs, leaf_cls=DOFArray, return_nested=nested)
 
 
 def weak_local_d_dx(dcoll: DiscretizationCollection, *args) -> ArrayOrContainerT:
@@ -544,8 +539,7 @@ def weak_local_div(dcoll: DiscretizationCollection, *args) -> ArrayOrContainerT:
             for i, vec_i in enumerate(vec)),
         in_shape=(dcoll.ambient_dim,),
         out_shape=(),
-        is_scalar=lambda v: isinstance(v, DOFArray),
-        ary=vecs)
+        ary=vecs, leaf_cls=DOFArray)
 
 # }}}
 
