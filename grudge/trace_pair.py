5aa6c1dd1d3d5712a8a14b24280e0cf4d1b60498
Matthew Smith <mjsmith6@illinois.edu>
2022-04-18 11:44:43 -0500

    don't try to create trace pair if there's no adjacency

--- grudge/trace_pair.py
+++ grudge/trace_pair.py
@@ -391,6 +391,12 @@ def local_inter_volume_trace_pairs(
     result: Mapping[Tuple[DOFDesc, DOFDesc], TracePair] = {}
 
     for vol_dd_pair, vol_data_pair in pairwise_volume_data.items():
+        from meshmode.mesh import mesh_has_boundary
+        if not mesh_has_boundary(
+                dcoll.discr_from_dd(vol_dd_pair[0]).mesh,
+                BTAG_PARTITION(PartID(vol_dd_pair[1].domain_tag.tag, rank))):
+            continue
+
         directional_vol_dd_pairs = [
             (vol_dd_pair[1], vol_dd_pair[0]),
             (vol_dd_pair[0], vol_dd_pair[1])]
