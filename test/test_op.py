683cdd839df7b48781eed59f1bdfc1fc6f0c5f3b
Addison Alvey-Blanco <aja13@illinois.edu>
2023-08-10 18:14:22 -0500

    Update tensor product divergence test to match original test case.

--- test/test_op.py
+++ test/test_op.py
@@ -169,7 +169,7 @@ def test_gradient(actx_factory, form, dim, order, vectorize, nested,
     (True, False),
     (True, True)
     ])
-def test_tensor_product_gradient(actx_factory, form, dim, order, vectorize, 
+def test_tensor_product_gradient(actx_factory, form, dim, order, vectorize,
                                  nested, visualize=False):
     """A "one-dimensional tensor product element" does not make sense, so the
     one-dimensional case is excluded from this test.
@@ -410,14 +410,17 @@ def test_divergence(actx_factory, form, dim, order, vectorize, nested,
 @pytest.mark.parametrize("dim", [2, 3])
 @pytest.mark.parametrize("order", [2, 3])
 @pytest.mark.parametrize(("vectorize", "nested"), [
-    (False, False)
+    (False, False),
+    (True, False),
+    (True, True)
     ])
 def test_tensor_product_divergence(actx_factory, form, dim, order, vectorize,
-                                 nested, visualize=False):
+                                   nested, visualize=False):
     """A "one-dimensional tensor product element" does not make sense, so the
     one-dimensional case is excluded from this test.
     """
     actx = actx_factory()
+
     from pytools.convergence import EOCRecorder
     eoc_rec = EOCRecorder()
 
@@ -426,63 +429,105 @@ def test_tensor_product_divergence(actx_factory, form, dim, order, vectorize,
         LegendreGaussLobattoTensorProductGroupFactory as LGL
     for n in [4, 6, 8]:
         mesh = mgen.generate_regular_rect_mesh(
-                a=(-1,)*dim,
-                b=(1,)*dim,
+                a=(-1,)*dim, b=(1,)*dim,
                 nelements_per_axis=(n,)*dim,
                 group_cls=TensorProductElementGroup)
 
         import grudge.dof_desc as dd
-        dcoll = make_discretization_collection(
+        dcoll = DiscretizationCollection(
                 actx,
                 mesh,
                 discr_tag_to_group_factory={
                     dd.DISCR_TAG_BASE: LGL(order)})
 
-
         def f(x):
-            if dim == 2:
-                ret = make_obj_array([dcoll.empty(actx) for _ in range(dim)])
-                ret[0] = actx.np.cos(np.pi*x[0])
-                ret[1] = actx.np.sin(np.pi*x[1])
-
-                return ret
-            elif dim == 3:
-                ret = make_obj_array([dcoll.empty(actx) for _ in range(dim)])
-                ret[0] = actx.np.cos(np.pi*x[0])
-                ret[1] = actx.np.sin(np.pi*x[1])
-                ret[2] = actx.np.sin(np.pi*x[2])
-
-                return ret
-
+            result = make_obj_array([dcoll.zeros(actx) + (i+1) for i in range(dim)])
+            for i in range(dim-1):
+                result = result * actx.np.sin(np.pi*x[i])
+            result = result * actx.np.cos(np.pi/2*x[dim-1])
+            return result
 
         def div_f(x):
-
-            if dim == 2:
-                ret = -np.pi*actx.np.sin(np.pi*x[0]) + \
-                        np.pi*actx.np.cos(np.pi*x[1])
-                return ret
-            elif dim == 3:
-                ret = -np.pi*actx.np.sin(np.pi*x[0]) + \
-                        np.pi*actx.np.cos(np.pi*x[1]) + \
-                        np.pi*actx.np.cos(np.pi*x[2])
-
-                return ret
-
+            result = dcoll.zeros(actx)
+            for i in range(dim-1):
+                deriv = dcoll.zeros(actx) + (i+1)
+                for j in range(i):
+                    deriv = deriv * actx.np.sin(np.pi*x[j])
+                deriv = deriv * np.pi*actx.np.cos(np.pi*x[i])
+                for j in range(i+1, dim-1):
+                    deriv = deriv * actx.np.sin(np.pi*x[j])
+                deriv = deriv * actx.np.cos(np.pi/2*x[dim-1])
+                result = result + deriv
+            deriv = dcoll.zeros(actx) + dim
+            for j in range(dim-1):
+                deriv = deriv * actx.np.sin(np.pi*x[j])
+            deriv = deriv * (-np.pi/2*actx.np.sin(np.pi/2*x[dim-1]))
+            result = result + deriv
+            return result
 
         x = actx.thaw(dcoll.nodes())
-        u = f(x)
-        ref_div = div_f(x)
-        div = op.local_div(dcoll, u)
 
-        rel_linf_error = actx.to_numpy(op.norm(dcoll, ref_div - div, np.inf) /
-                                       op.norm(dcoll, ref_div, np.inf))
-        eoc_rec.add_data_point(1./n, rel_linf_error)
+        if vectorize:
+            u = make_obj_array([(i+1)*f(x) for i in range(dim)])
+            if not nested:
+                u = np.stack(u, axis=0)
+        else:
+            u = f(x)
+
+        def get_flux(u_tpair):
+            dd = u_tpair.dd
+            dd_allfaces = dd.with_dtag("all_faces")
+            normal = actx.thaw(dcoll.normal(dd))
+            flux = u_tpair.avg @ normal
+            return op.project(dcoll, dd, dd_allfaces, flux)
+
+        dd_allfaces = DOFDesc("all_faces")
+
+        if form == "strong":
+            div_u = (
+                op.local_div(dcoll, u)
+                # No flux terms because u doesn't have inter-el jumps
+                )
+        elif form == "weak":
+            div_u = op.inverse_mass(dcoll,
+                -op.weak_local_div(dcoll, u)
+                +  # noqa: W504
+                op.face_mass(dcoll,
+                    dd_allfaces,
+                    # Note: no boundary flux terms here because u_ext == u_int == 0
+                    sum(get_flux(utpair)
+                        for utpair in op.interior_trace_pairs(dcoll, u))
+                )
+            )
+        else:
+            raise ValueError("Invalid form argument.")
+
+        if vectorize:
+            expected_div_u = make_obj_array([(i+1)*div_f(x) for i in range(dim)])
+        else:
+            expected_div_u = div_f(x)
+
+        if visualize:
+            from grudge.shortcuts import make_visualizer
+            vis = make_visualizer(dcoll, vis_order=order if dim == 3 else dim+3)
+
+            filename = (f"test_divergence_{form}_{dim}_{order}"
+                f"{'_vec' if vectorize else ''}{'_nested' if nested else ''}.vtu")
+            vis.write_vtk_file(filename, [
+                ("u", u),
+                ("div_u", div_u),
+                ("expected_div_u", expected_div_u),
+                ], overwrite=True)
+
+        rel_linf_err = actx.to_numpy(
+            op.norm(dcoll, div_u - expected_div_u, np.inf)
+            / op.norm(dcoll, expected_div_u, np.inf))
+        eoc_rec.add_data_point(1./n, rel_linf_err)
 
     print("L^inf error:")
     print(eoc_rec)
-    assert (eoc_rec.order_estimate() >= order - 0.5 or
-            eoc_rec.max_error() < 1e-11)
-
+    assert (eoc_rec.order_estimate() >= order - 0.5
+                or eoc_rec.max_error() < 1e-11)
 # }}}
 
 
