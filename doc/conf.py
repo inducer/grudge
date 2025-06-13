470a95cc7d771a7588dda2c7e339cf32918bc14d
Alexandru Fikl <alexfikl@gmail.com>
2025-06-13 15:28:40 +0300

    docs: use versioned sphinxconfig

--- doc/conf.py
+++ doc/conf.py
@@ -3,8 +3,7 @@ from importlib import metadata
 from urllib.request import urlopen
 
 
-_conf_url = \
-        "https://raw.githubusercontent.com/inducer/sphinxconfig/main/sphinxconfig.py"
+_conf_url = "https://tiker.net/sphinxconfig-v0.py"
 with urlopen(_conf_url) as _inf:
     exec(compile(_inf.read(), _conf_url, "exec"), globals())
 
@@ -17,15 +16,15 @@ release = metadata.version("grudge")
 version = ".".join(release.split(".")[:2])
 
 intersphinx_mapping = {
-        "arraycontext": ("https://documen.tician.de/arraycontext/", None),
-        "loopy": ("https://documen.tician.de/loopy/", None),
-        "meshmode": ("https://documen.tician.de/meshmode/", None),
-        "modepy": ("https://documen.tician.de/modepy/", None),
-        "mpi4py": ("https://mpi4py.readthedocs.io/en/stable", None),
-        "numpy": ("https://numpy.org/doc/stable/", None),
-        "pymbolic": ("https://documen.tician.de/pymbolic/", None),
-        "pyopencl": ("https://documen.tician.de/pyopencl/", None),
-        "python": ("https://docs.python.org/3/", None),
+    "arraycontext": ("https://documen.tician.de/arraycontext/", None),
+    "loopy": ("https://documen.tician.de/loopy/", None),
+    "meshmode": ("https://documen.tician.de/meshmode/", None),
+    "modepy": ("https://documen.tician.de/modepy/", None),
+    "mpi4py": ("https://mpi4py.readthedocs.io/en/stable", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "pymbolic": ("https://documen.tician.de/pymbolic/", None),
+    "pyopencl": ("https://documen.tician.de/pyopencl/", None),
+    "python": ("https://docs.python.org/3/", None),
 }
 
 # index-page demo uses pyopencl via plot_directive
